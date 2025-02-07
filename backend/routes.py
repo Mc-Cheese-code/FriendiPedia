6ff2f33b4ef7b92bf4dc3e432e72d570263ef939
Mc-Cheese-code <sidd.patty@gmail.com>
2025-02-07 22:56:57 +0530

    first commit

---
+++ backend/routes.py
@@ -0,0 +1,9 @@
+from app import app, db
+from flask import Flask, jsonify
+from models import Friend
+
+@app.route("/api/friends",methods=["GET"])
+def get_friends():
+    friends = Friend.query.all()
+    result = [friend.to_json() for friend in friends]
+    return jsonify(result)
